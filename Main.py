9966822a79a0f447fd7929518c5dc41310c2e2c5
Reyan <reyanjaved343@gmail.com>
2024-12-31 15:12:22 +0000

    Removed paused button and combined with play button, started switching from pygame mixer to QMediaPlayer for music playback

--- Main.py
+++ Main.py
@@ -4,7 +4,8 @@ from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, 
     QPushButton, QFileDialog, QListWidget, QLabel, QMessageBox
 )
-from pygame import mixer
+from PyQt5.QtMultimedia import QMediaPlayer
+#from pygame import mixer
 
 class Window(QMainWindow):
     def __init__(self):
@@ -15,14 +16,15 @@ class Window(QMainWindow):
         self.setCentralWidget(central_widget)
         
         layout = QHBoxLayout()
-        self.playButton = QPushButton("Play", self)
-        self.pauseButton = QPushButton("Pause", self)
+        self.playButton = QPushButton("Pause Play", self)
+        self.playButton.setCheckable(True)
+        #self.pauseButton = QPushButton("Pause", self)
         self.openFileButton = QPushButton("Choose", self)
         
         self.songlist = QListWidget()
         
         layout.addWidget(self.playButton)
-        layout.addWidget(self.pauseButton)
+        #layout.addWidget(self.pauseButton)
         layout.addWidget(self.openFileButton)
         
         layout.addWidget(self.songlist)
@@ -30,7 +32,8 @@ class Window(QMainWindow):
         central_widget.setLayout(layout)
         
         self.openFileButton.clicked.connect(self.chooseFolder)
-        self.playButton.clicked.connect(self.playSong)
+        self.playButton.clicked.connect(self.pausePlay)
+        #self.pauseButton.clicked.connect(self.pausePlay)
         
     def chooseFolder(self):
         self.folder = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
@@ -45,17 +48,29 @@ class Window(QMainWindow):
             error_dialog = QWidget.QErrorMessage()
             error_dialog.showMessage('Please select a folder containing music')
             
-    def playSong(self):
+    def playSong(self):        
         self.songToPlay = os.path.join(self.folder, self.songlist.currentItem().text()) # not sure why currentItem doesnt get the location as well
         #self.songLocation = os.path.join(self.folder, self.songToPlay)
         #print(self.songToPlay)
         
+        # Use QMediaPlayer instead of mixer
+        
         mixer.init()
         
         mixer.music.load(self.songToPlay)
-        
         mixer.music.play()
         
+    def pausePlay(self):
+        self.playSong()
+        if self.playButton.isChecked():
+            self.isPaused = False
+        
+        if self.isPaused:
+            mixer.music.unpause()
+            self.isPaused = False
+        else:
+            mixer.music.pause()
+            self.isPaused = True
         
 app = QApplication(sys.argv)
 window = Window()
